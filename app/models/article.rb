4eaa40721a79e7ac28829240a3904b566536e6a3
Ng Guoyou <fauxface+github@gmail.com>
2016-05-22 11:56:14 +0800

    Use simple_format to sanitize article content

--- app/models/article.rb
+++ app/models/article.rb
@@ -3,8 +3,6 @@ class Article < ActiveRecord::Base
   before_save :sanitize_content
 
   def sanitize_content
-    self.content = ActionController::Base.helpers.sanitize(content,
-      tags: %w(a ul li ol strong em code pre br marquee img video iframe),
-      attributes: %w(href class style width height src frameborder allowfullscreen))
+    self.content = ActionController::Base.helpers.simple_format(content)
   end
 end
