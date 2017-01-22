cb0770e14241aa88e57215c8c642013f1f073bef
Ng Guoyou <fauxface+github@gmail.com>
2017-01-22 15:42:38 +0800

    Fix broken migration file

--- db/migrate/20151127084803_create_songs.rb
+++ db/migrate/20151127084803_create_songs.rb
@@ -1,7 +1,7 @@
 class CreateSongs < ActiveRecord::Migration
   def change
     create_table :songs do |t|
-      t.references :artist, index: true, foreign_key: true
+      t.string :artist, index: true
       t.string :title
 
       t.timestamps null: false
