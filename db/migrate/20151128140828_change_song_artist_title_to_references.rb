1617b3d795f689ee8f84bcaedb12a89c31eebc70
Ng Guoyou <fauxface+github@gmail.com>
2017-01-22 15:45:38 +0800

    Fix broken migration with artist column on songs, pt. 2

--- db/migrate/20151128140828_change_song_artist_title_to_references.rb
+++ db/migrate/20151128140828_change_song_artist_title_to_references.rb
@@ -1,7 +1,6 @@
 class ChangeSongArtistTitleToReferences < ActiveRecord::Migration
   def change
     remove_column :songs, :artist
-    remove_column :songs, :artist_id
     remove_column :songs, :title
     add_column :songs, :artist_metadata_id, :integer
     add_column :songs, :title_metadata_id, :integer
